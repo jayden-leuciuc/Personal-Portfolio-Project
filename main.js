f10daa62f1c85f95574a3708e3ff9d730e6329ba
Jayden Leuciuc <leuciucjayden@gmail.com>
2020-11-10 10:29:02 -0500

    Created animation, cleaned up js

--- main.js
+++ main.js
@@ -1,99 +1,67 @@
-const htmlIc = document.querySelector('.fa-html5');
 const htmlInfo = document.querySelector('.html-block');
-
-const cssIc = document.querySelector('.fa-css3-alt');
 const cssInfo = document.querySelector('.css-block');
-
-const jsIc = document.querySelector('.fa-js-square');
 const jsInfo = document.querySelector('.js-block');
-
-const nodeIc = document.querySelector('.fa-node-js');
 const nodeInfo = document.querySelector('.node-block');
-
-const sassIc = document.querySelector('.fa-sass');
 const sassInfo = document.querySelector('.sass-block');
-
-const reactIc = document.querySelector('.fa-react');
 const reactInfo = document.querySelector('.react-block');
-
-const gitIc = document.querySelector('.fa-git-square');
 const gitInfo = document.querySelector('.git-block');
-
-htmlIc.addEventListener("click", toggleHtml);
-cssIc.addEventListener("click", toggleCss);
-jsIc.addEventListener("click", toggleJs);
-nodeIc.addEventListener("click", toggleNode);
-sassIc.addEventListener("click", toggleSass);
-reactIc.addEventListener("click", toggleReact);
-gitIc.addEventListener("click", toggleGit);
+let lastElement = null;
 
 function toggleHtml() {
-       htmlInfo.classList.toggle("show");
-       cssInfo.classList.remove("show");
-       jsInfo.classList.remove("show");
-       nodeInfo.classList.remove("show");
-       sassInfo.classList.remove("show");
-       reactInfo.classList.remove("show");
-       gitInfo.classList.remove("show");
+       htmlInfo.classList.add("show");
+       if(lastElement != null) {
+              lastElement.classList.remove("show");
+       }
+       
+       lastElement = htmlInfo;
+       
 }
 
 function toggleCss() {
-       htmlInfo.classList.remove("show");
-       cssInfo.classList.toggle("show");
-       jsInfo.classList.remove("show");
-       nodeInfo.classList.remove("show");
-       sassInfo.classList.remove("show");
-       reactInfo.classList.remove("show");
-       gitInfo.classList.remove("show");
+       cssInfo.classList.add("show");
+       if(lastElement != null) {
+              lastElement.classList.remove("show");
+       }
+       lastElement = cssInfo;
 }
 
 function toggleJs() {
-       htmlInfo.classList.remove("show");
-       cssInfo.classList.remove("show");
-       jsInfo.classList.toggle("show");
-       nodeInfo.classList.remove("show");
-       sassInfo.classList.remove("show");
-       reactInfo.classList.remove("show");
-       gitInfo.classList.remove("show");
+       jsInfo.classList.add("show");
+       if(lastElement != null) {
+              lastElement.classList.remove("show");
+       }
+       lastElement = jsInfo;
 }
 
 function toggleNode() {
-       htmlInfo.classList.remove("show");
-       cssInfo.classList.remove("show");
-       jsInfo.classList.remove("show");
-       nodeInfo.classList.toggle("show");
-       sassInfo.classList.remove("show");
-       reactInfo.classList.remove("show");
-       gitInfo.classList.remove("show");
+       nodeInfo.classList.add("show");
+       if(lastElement != null) {
+              lastElement.classList.remove("show");
+       }
+       lastElement = nodeInfo;
 }
 
 function toggleSass() {
-       htmlInfo.classList.remove("show");
-       cssInfo.classList.remove("show");
-       jsInfo.classList.remove("show");
-       nodeInfo.classList.remove("show");
-       sassInfo.classList.toggle("show");
-       reactInfo.classList.remove("show");
-       gitInfo.classList.remove("show");
+       sassInfo.classList.add("show");
+       if(lastElement != null) {
+              lastElement.classList.remove("show");
+       }
+       lastElement = sassInfo;
 }
 
 function toggleReact() {
-       htmlInfo.classList.remove("show");
-       cssInfo.classList.remove("show");
-       jsInfo.classList.remove("show");
-       nodeInfo.classList.remove("show");
-       sassInfo.classList.remove("show");
-       reactInfo.classList.toggle("show");
-       gitInfo.classList.remove("show");
+       reactInfo.classList.add("show");
+       if(lastElement != null) {
+              lastElement.classList.remove("show");
+       }
+       lastElement = reactInfo;
 }
 function toggleGit() {
-       htmlInfo.classList.remove("show");
-       cssInfo.classList.remove("show");
-       jsInfo.classList.remove("show");
-       nodeInfo.classList.remove("show");
-       sassInfo.classList.remove("show");
-       reactInfo.classList.remove("show");
-       gitInfo.classList.toggle("show");
+       gitInfo.classList.add("show");
+       if(lastElement != null) {
+              lastElement.classList.remove("show");
+       }
+       lastElement = gitInfo;
 }
 
 
